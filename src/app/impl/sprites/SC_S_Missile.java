3d5bf6675fc953aabef74f73876bccefc91d3b11
OPUee <lukas.luschin@gmail.com>
2017-04-24 21:50:27 +0200

    add sprites implementation for first game experience

---
+++ src/app/impl/sprites/SC_S_Missile.java
@@ -0,0 +1,43 @@
+package app.impl.sprites;
+
+import lowbob.LowBobSprite;
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+
+/**
+ * Created by opuee on 24.04.17.
+ */
+public class SC_S_Missile extends LowBobSprite {
+
+    private final int SPEED = 15;
+    private final double ACCELE = .3;
+
+    public SC_S_Missile(double x, double y) {
+        super(x,y);
+    }
+
+    @Override
+    public void loadImage() {
+        this.img = new ImageIcon("src/app/resources/missile.png").getImage();
+    }
+
+    @Override
+    public void move() {
+
+        if(vx < SPEED)
+            vx += ACCELE;
+
+        x += vx;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+
+    }
+}
---
+++ src/app/impl/sprites/SC_S_SpaceCraft.java
@@ -0,0 +1,92 @@
+package app.impl.sprites;
+
+import lowbob.LowBobSprite;
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+
+/**
+ * Created by opuee on 24.04.17.
+ */
+public class SC_S_SpaceCraft extends LowBobSprite{
+
+    private final double SPEED = 5;
+    private final double ACCELE = .2;
+
+    private boolean up, down, fired;
+
+    public SC_S_SpaceCraft(int x, int y) {
+        super(x, y);
+
+        // initialize member
+        up = false;
+        down = false;
+        fired = false;
+    }
+
+    public void fire() {
+        this.addSprite(new SC_S_Missile(this.x+25, this.y+18));
+        fired = true;
+    }
+
+    @Override
+    public void loadImage() {
+        this.img = new ImageIcon("src/app/resources/spacecraft.png").getImage();
+    }
+
+    @Override
+    public void move() {
+        //smoothing for velocity;
+        if(up) {
+            vy -= ACCELE;
+            if(vy <= -SPEED)
+                vy = -SPEED;
+        }
+        else {
+            if(vy < 0)
+                vy += ACCELE;
+        }
+
+        if(down) {
+            vy += ACCELE;
+            if(vy >= SPEED)
+                vy  = SPEED;
+        }
+        else {
+            if(vy > 0)
+                vy -= ACCELE;
+        }
+
+        // set absolute position
+        this.x += this.vx;
+        this.y += this.vy;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+        switch(keyEvent.getKeyCode()) {
+            case KeyEvent.VK_UP:
+                up = true;
+                break;
+            case KeyEvent.VK_DOWN:
+                down = true;
+                break;
+            case KeyEvent.VK_SPACE:
+                if(!fired)
+                fire();
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+        switch(keyEvent.getKeyCode()) {
+            case KeyEvent.VK_UP:
+                up = false;
+            case KeyEvent.VK_DOWN:
+                down = false;
+            case KeyEvent.VK_SPACE:
+                fired = false;
+        }
+    }
+}
