823f0a38303dfedfca48b31fbf62daf9580f73ad
Lukas Luschin <lukas.luschin@web.de>
2017-04-28 21:56:38 +0200

    add content for technical preview

--- src/app/impl/panels/SC_P_Debug.java
+++ src/app/impl/panels/SC_P_Debug.java
@@ -1,19 +1,25 @@
 package app.impl.panels;
 
 import app.impl.sprites.SC_S_Alien;
+import app.impl.sprites.SC_S_Alien_Thurster;
 import app.impl.sprites.SC_S_Coin;
 import app.impl.sprites.SC_S_SpaceCraft;
 import app.impl.sprites.SC_S_Star;
 import app.impl.sprites.SC_S_Thurster;
 import lowbob.LowBobPanel;
 import java.awt.*;
+import java.util.Random;
 
 /**
  * Created by opuee on 24.04.17.
  */
 public class SC_P_Debug extends LowBobPanel {
+	
+	private Random rnd;
 
     public SC_P_Debug() {
+    	rnd = new Random();
+    	
         // init panel
         setBackground(Color.BLACK);
         setPreferredSize(new Dimension(1400, 800));
@@ -23,9 +29,9 @@ public class SC_P_Debug extends LowBobPanel {
         this.addSprite(player);
         this.addSprite(new SC_S_Thurster(-16, 18, 15, 7, player));
         
-        // add scene resources
-        this.addSprite(new SC_S_Alien(800, 400, 50, 26));
-        this.addSprite(new SC_S_Star(1000, 700, 16, 16));
-        this.addSprite(new SC_S_Coin(500, 500, 14, 14));
+        // add alien enemy
+        SC_S_Alien alien = new SC_S_Alien(1000, 400, 50, 26);
+        this.addSprite(alien);
+        this.addSprite(new SC_S_Alien_Thurster(45, 8, 20, 14, alien));
     }
 }
---
+++ src/app/impl/sprites/SC_S_Alien_Thurster.java
@@ -0,0 +1,60 @@
+package app.impl.sprites;
+
+import java.awt.event.KeyEvent;
+
+import lowbob.LowBobSprite;
+import lowbob.util.ImageAnimator;
+import lowbob.util.ImageCreator;
+
+public class SC_S_Alien_Thurster extends LowBobSprite {
+
+	private ImageAnimator ai_thurster;
+	private LowBobSprite parent;
+	private int ai_counter;
+	private double xOffset, yOffset;
+	
+	public SC_S_Alien_Thurster(double x, double y, double width, double height, LowBobSprite parent) {
+		super(x, y, width, height);
+		
+		this.xOffset = x;
+		this.yOffset = y;
+		this.ai_counter = 0;
+		this.parent = parent;
+	}
+
+	@Override
+	public void loadImage() {
+		ai_thurster = new ImageAnimator(ImageCreator.create("src/app/resources/alien_flame.png"), 20);
+		this.img = ai_thurster.next();
+	}
+
+	@Override
+	public void move() {			
+		x = parent.getPosX() + xOffset;
+		y = parent.getPosY() + yOffset;
+		
+		ai_counter++;
+		// update image
+		if(ai_counter > 1000)
+			ai_counter = 0;
+		if((ai_counter % 5) == 0)
+			this.img = ai_thurster.next();
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent keyEvent) {
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent keyEvent) {
+		
+	}
+
+	@Override
+	public void collide(LowBobSprite lbs) {
+		
+	}
+
+}
