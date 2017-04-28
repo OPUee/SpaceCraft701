370bfb0a58aca363149ada98763be113ac20f0c1
Lukas Luschin <lukas.luschin@web.de>
2017-04-28 21:41:38 +0200

    add thurster to players spacecraft

--- src/app/impl/panels/SC_P_Debug.java
+++ src/app/impl/panels/SC_P_Debug.java
@@ -4,6 +4,7 @@ import app.impl.sprites.SC_S_Alien;
 import app.impl.sprites.SC_S_Coin;
 import app.impl.sprites.SC_S_SpaceCraft;
 import app.impl.sprites.SC_S_Star;
+import app.impl.sprites.SC_S_Thurster;
 import lowbob.LowBobPanel;
 import java.awt.*;
 
@@ -17,7 +18,12 @@ public class SC_P_Debug extends LowBobPanel {
         setBackground(Color.BLACK);
         setPreferredSize(new Dimension(1400, 800));
 
-        this.addSprite(new SC_S_SpaceCraft(40,120, 50, 30));
+        // add spacecraft player
+        SC_S_SpaceCraft player = new SC_S_SpaceCraft(40,120, 50, 30);
+        this.addSprite(player);
+        this.addSprite(new SC_S_Thurster(-16, 18, 15, 7, player));
+        
+        // add scene resources
         this.addSprite(new SC_S_Alien(800, 400, 50, 26));
         this.addSprite(new SC_S_Star(1000, 700, 16, 16));
         this.addSprite(new SC_S_Coin(500, 500, 14, 14));
