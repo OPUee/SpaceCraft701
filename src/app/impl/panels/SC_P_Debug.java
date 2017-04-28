9548d9f3ff0152ad7601fe5af0cfa6a36160ff78
OPUee <lukas.luschin@gmail.com>
2017-04-28 12:57:18 +0200

    implement star class for testing value animator

--- src/app/impl/panels/SC_P_Debug.java
+++ src/app/impl/panels/SC_P_Debug.java
@@ -2,6 +2,7 @@ package app.impl.panels;
 
 import app.impl.sprites.SC_S_Alien;
 import app.impl.sprites.SC_S_SpaceCraft;
+import app.impl.sprites.SC_S_Star;
 import lowbob.LowBobPanel;
 import java.awt.*;
 
@@ -16,6 +17,7 @@ public class SC_P_Debug extends LowBobPanel {
         setPreferredSize(new Dimension(1400, 800));
 
         this.addSprite(new SC_S_SpaceCraft(40,120, 50, 30));
-        this.addSprite(new SC_S_Alien(800,400, 50, 26));
+        this.addSprite(new SC_S_Alien(800, 400, 50, 26));
+        this.addSprite(new SC_S_Star(1000, 700, 16, 16));
     }
 }
