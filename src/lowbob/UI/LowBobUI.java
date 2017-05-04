d067f4a54fd3f9eec99dbfbc7faeb6372352902d
OPUee <lukas.luschin@gmail.com>
2017-05-04 23:10:56 +0200

    add UI abstraction

--- src/lowbob/UI/LowBobUI.java
+++ src/lowbob/UI/LowBobUI.java
@@ -1,7 +1,14 @@
 package lowbob.UI;
 
+import lowbob.LowBobSprite;
+
+import java.awt.event.KeyEvent;
+
 /**
  * Created by opuee on 02.05.17.
  */
-public class LowBobUI {
+public abstract class LowBobUI extends LowBobSprite {
+    public LowBobUI(double x, double y, double width, double height) {
+        super(x, y, width, height);
+    }
 }
