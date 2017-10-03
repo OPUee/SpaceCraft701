acdb78e6de877faf75ae44c019ce51aa9b210ff0
OPUee <lukas.luschin@gmail.com>
2017-10-03 19:49:36 +0200

    implement a directed behavior with linear alteration

--- src/lowbob/particles/impl/LowBobDirectedBehavior.java
+++ src/lowbob/particles/impl/LowBobDirectedBehavior.java
@@ -1,5 +1,6 @@
 package lowbob.particles.impl;
 
+import com.sun.javafx.geom.Vec2d;
 import lowbob.particles.LowBobParticle;
 import lowbob.particles.LowBobParticleBehavior;
 
@@ -10,10 +11,19 @@ import java.awt.*;
  */
 public class LowBobDirectedBehavior implements LowBobParticleBehavior {
 
+    private Vec2d m_direction;
 
+    public LowBobDirectedBehavior(Vec2d direction) {
+        this.m_direction = direction;
+    }
 
     @Override
-    public Point update(LowBobParticle particle) {
-        return null;
+    public Vec2d update(Vec2d From, float offset) {
+        Vec2d result = new Vec2d();
+
+        result.x = From.x + ((this.m_direction.x - From.x) * offset);
+        result.y = From.y + ((this.m_direction.y - From.y) * offset);
+
+        return result;
     }
 }
