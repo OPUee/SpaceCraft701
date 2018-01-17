3dbc7c5a7a718362b088777e69f2f5a9807e0f5b
Lukas <lukas.luschin@htwg-konstanz.de>
2018-01-17 21:12:33 +0100

    add further sprites

---
+++ src/app/impl/sprites/SC_S_Boomer.java
@@ -0,0 +1,7 @@
+package app.impl.sprites;
+
+/**
+ * Created by opuee on 10.10.17.
+ */
+public class SC_S_Boomer {
+}
---
+++ src/app/impl/sprites/SC_S_Plutonium.java
@@ -0,0 +1,7 @@
+package app.impl.sprites;
+
+/**
+ * Created by opuee on 11.10.17.
+ */
+public class SC_S_Plutonium {
+}
