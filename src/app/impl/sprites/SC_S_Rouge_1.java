155f3c75f144a2af395015cd904903f48f2a1097
Lukas Luschin <lukas.luschin@gmail.com>
2019-02-27 18:09:28 +0100

    new enemy

--- src/app/impl/sprites/SC_S_Rouge_1.java
+++ src/app/impl/sprites/SC_S_Rouge_1.java
@@ -7,10 +7,18 @@ public class SC_S_Rouge_1 extends LowBobSprite {
 
     public SC_S_Rouge_1(double x, double y, double width, double height, int z) {
         super(x, y, width, height, z);
+
+        this.addSprite(new SC_S_Shield_Generator(-75,-2,0,0,2));
     }
 
     @Override
     public void loadImage() {
-        this.img = ImageCreator.create("resources/pics/rogue_01.png");
+        this.img = ImageCreator.create("resources/pics/rogue_02.png");
     }
+
+    @Override
+    public void move() {
+
+    }
+
 }
--- src/app/impl/sprites/SC_S_Shield_Generator.java
+++ src/app/impl/sprites/SC_S_Shield_Generator.java
@@ -1,4 +1,17 @@
 package app.impl.sprites;
 
-public class SC_S_Shield_Generator {
+import lowbob.LowBobSprite;
+import lowbob.util.ImageCreator;
+
+public class SC_S_Shield_Generator extends LowBobSprite {
+
+    public SC_S_Shield_Generator(double x, double y, double width, double height, int z) {
+        super(x, y, width, height, z);
+    }
+
+    @Override
+    public void loadImage() {
+        this.img = ImageCreator.create("resources/pics/shield_generator.png");
+    }
+
 }
